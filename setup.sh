eaf213df970333dcf64726d1b9dcf23cc5c3895b
Marcel Wysocki <maci.stgn@gmail.com>
2013-12-17 11:53:55 +0100

    minor fix

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ set -e
 SCRIPTPATH=`echo $(dirname $(readlink -f $0))`
 cd "${SCRIPTPATH}"
 
+mkdir upstream output
+
 SRC_ISO="http://ftp.redhat.com/redhat/rhel/beta/7/source/iso/rhel-everything-7.0-beta-1-source-dvd.iso"
 BIN_ISO="http://ftp.redhat.com/redhat/rhel/beta/7/x86_64/iso/rhel-everything-7.0-beta-1-x86_64-dvd.iso"
 
@@ -25,3 +27,4 @@ sudo cp epel-7-x86_64.cfg /etc/mock
 
 sudo sed -i 's@RHEL7BETA@'${BIN_PATH}'@' /etc/mock/epel-7-x86_64.cfg
 sudo sed -i 's@OUTPUT@'${SCRIPTPATH}'/output@' /etc/mock/epel-7-x86_64.cfg
+
